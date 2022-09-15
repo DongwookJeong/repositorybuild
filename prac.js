4d8f055b668f6e0cc4ee9b4f35d1de8251b3b010
jungdongwook <nolhoon211@gmail.com>
2022-09-15 19:02:58 +0900

    park jaehyeong

---
+++ prac.js
@@ -0,0 +1,13 @@
+let a = 1;
+let b = 2;
+
+let c = [3, 4, 5];
+
+function sum(first, second){
+    return first + second
+}
+
+console.log(sum(a,b));
+let test = sum(a,b)
+const map1 = c.map(x => x * 2);
+console.log(map1)
